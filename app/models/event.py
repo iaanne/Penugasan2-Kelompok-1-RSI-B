e7b72d3b284ea02e84d9584daa1295b899195c95
Jocelyn <jocelynlouisa22@gmail.com>
2026-04-09 16:13:33 +0700

    fix models event.py

--- app/models/event.py
+++ app/models/event.py
@@ -6,9 +6,6 @@ class Event(Base):
     __tablename__ = "events"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    title = Column(String, nullable=False)
     description = Column(String)
-    quota = Column(Integer, nullable=False)
-    started_at = Column(DateTime, nullable=False)
-    ended_at = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    date = Column(DateTime, default=datetime.utcnow)
