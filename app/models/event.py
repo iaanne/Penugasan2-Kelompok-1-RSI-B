6c563dface1acdaed340295b340010a2ae5f37f3
Jocelyn <jocelynlouisa22@gmail.com>
2026-04-09 16:06:02 +0700

    fix models event.py

--- app/models/event.py
+++ app/models/event.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from app.database import Base
-from datetime import datetime
 
 class Event(Base):
     __tablename__ = "events"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     description = Column(String)
-    date = Column(DateTime, default=datetime.utcnow)
+    quota = Column(Integer, nullable=False)
+    started_at = Column(DateTime, nullable=False)
+    ended_at = Column(DateTime, nullable=False)
